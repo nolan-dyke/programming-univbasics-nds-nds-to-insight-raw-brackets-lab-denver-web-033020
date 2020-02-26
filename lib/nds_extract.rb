637a7dfb1b39a74156826cdcf974b89bd0cfa061
Nolan Dyke <nolan.g.dyke@gmail.com>
2020-02-26 23:22:02 +0000

    Automatically backed up by Learn

--- lib/nds_extract.rb
+++ lib/nds_extract.rb
@@ -7,11 +7,11 @@ pp directors_database
 def directors_totals(nds)
   total_hash = {}
   array_index = 0 
-  while array_index < directors_database.length do
+  while array_index < nds.length do
     object_index = 0 
     director_name = nds[array_index][:name]
     director_sum = 0 
-    while object_index < directors_database[array_index].length do 
+    while object_index < nds[array_index].length do 
       
       object_index += 1 
     end 
